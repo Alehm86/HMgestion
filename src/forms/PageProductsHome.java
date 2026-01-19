0c03bd6a3172c407cfee7b3a7eaf6da3a3352f5d
Alehm86 <alehmartin1986@gmail.com>
2026-01-19 16:46:26 -0300

    V1.2 - Modificaciones en formulario, modulo productos

--- src/forms/PageProductsHome.java
+++ src/forms/PageProductsHome.java
@@ -57,50 +57,54 @@ public class PageProductsHome extends javax.swing.JPanel {
             start();
         });
         
-//        btnRerturnProduct.addActionListener(e-> {
-//            frmReturnProducts returnProducts = new frmReturnProducts();
-//            returnProducts.setVisible(true);
-////            this.dispose();
-//        });       
-//        
-//        btnAddStock.addActionListener(e -> {
-//            frmStockManagement stockMgmt = new frmStockManagement();
-//            stockMgmt.setVisible(true);
-////            this.dispose();
-//        });          
+        btnRerturnProduct.addActionListener(e-> {
+            PageProductReturn pReturn = new PageProductReturn();
+            pReturn.setSize(1700, 800);
+            pReturn.setLocation(0, 0);
+
+            jPanelProductContent.removeAll();
+            jPanelProductContent.add(pReturn, BorderLayout.CENTER);
+            jPanelProductContent.revalidate();
+            jPanelProductContent.repaint(); 
+        });       
+        
+        btnAddStock.addActionListener(e -> {
+            PageProductStock pStock = new PageProductStock();
+            pStock.setSize(1700, 800);
+            pStock.setLocation(0, 0);
+
+            jPanelProductContent.removeAll();
+            jPanelProductContent.add(pStock, BorderLayout.CENTER);
+            jPanelProductContent.revalidate();
+            jPanelProductContent.repaint(); 
+        });          
         
         btnNewProduct.addActionListener(e->{
 //            frmNewProduct edit = new frmNewProduct();
 //            edit.setVisible(true);
 //            this.dispose();
-            PageProductNew1 p2 = new PageProductNew1();
-            p2.setSize(1700, 800);
-            p2.setLocation(0, 0);
+            PageProductNew pNew = new PageProductNew();
+            pNew.setSize(1700, 800);
+            pNew.setLocation(0, 0);
 
             jPanelProductContent.removeAll();
-            jPanelProductContent.add(p2, BorderLayout.CENTER);
+            jPanelProductContent.add(pNew, BorderLayout.CENTER);
             jPanelProductContent.revalidate();
             jPanelProductContent.repaint();            
 
         });
         
-//        btnEdit.addActionListener(e->{
-//            int id = queries.obtenerIdProduct(filaSeleccionada);
-//
-//            frmEditProduct edit = new frmEditProduct();
-//            edit.dialogoEdit(id);
-//            edit.setVisible(true);
-////            this.dispose();  
-//        });
-//        
-//        btnEditPrice.addActionListener(e->{
-//            int id = queries.obtenerIdProduct(filaSeleccionada);
-//            
-//            frmEditPrice editPrice = new frmEditPrice();
-//            editPrice.dialogoEdit(id);
-//            editPrice.setVisible(true);
-//        });
-//        
+        btnEdit.addActionListener(e->{
+            PageProductEdit pEdit = new PageProductEdit();
+            pEdit.setSize(1700, 800);
+            pEdit.setLocation(0, 0);
+
+            jPanelProductContent.removeAll();
+            jPanelProductContent.add(pEdit, BorderLayout.CENTER);
+            jPanelProductContent.revalidate();
+            jPanelProductContent.repaint(); 
+        });
+      
     }
     
     
@@ -111,11 +115,10 @@ public class PageProductsHome extends javax.swing.JPanel {
 
         jPanel10 = new javax.swing.JPanel();
         btnNewProduct = new javax.swing.JButton();
-        btnEdit = new javax.swing.JButton();
-        btnEditPrice = new javax.swing.JButton();
         btnRerturnProduct = new javax.swing.JButton();
         btnAddStock = new javax.swing.JButton();
         BtnProdHome = new javax.swing.JButton();
+        btnEdit = new javax.swing.JButton();
         jPanelProductContent = new javax.swing.JPanel();
 
         setBackground(new java.awt.Color(255, 255, 255));
@@ -142,51 +145,6 @@ public class PageProductsHome extends javax.swing.JPanel {
         btnNewProduct.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnNewProductActionPerformed(evt);
-            }
-        });
-
-        btnEdit.setBackground(new java.awt.Color(255, 255, 255));
-        btnEdit.setFont(new java.awt.Font("Poppins", 1, 14)); // NOI18N
-        btnEdit.setForeground(new java.awt.Color(12, 83, 151));
-        btnEdit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons/ProductEdit.png"))); // NOI18N
-        btnEdit.setText("Editar");
-        btnEdit.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
-        btnEdit.setBorderPainted(false);
-        btnEdit.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        btnEdit.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                btnEditMouseEntered(evt);
-            }
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                btnEditMouseExited(evt);
-            }
-        });
-        btnEdit.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnEditActionPerformed(evt);
-            }
-        });
-
-        btnEditPrice.setBackground(new java.awt.Color(255, 255, 255));
-        btnEditPrice.setFont(new java.awt.Font("Poppins", 1, 14)); // NOI18N
-        btnEditPrice.setForeground(new java.awt.Color(12, 83, 151));
-        btnEditPrice.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons/ProductPrice.png"))); // NOI18N
-        btnEditPrice.setText("Precio");
-        btnEditPrice.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
-        btnEditPrice.setBorderPainted(false);
-        btnEditPrice.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        btnEditPrice.setFocusable(false);
-        btnEditPrice.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                btnEditPriceMouseEntered(evt);
-            }
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                btnEditPriceMouseExited(evt);
-            }
-        });
-        btnEditPrice.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnEditPriceActionPerformed(evt);
             }
         });
 
@@ -258,6 +216,28 @@ public class PageProductsHome extends javax.swing.JPanel {
             }
         });
 
+        btnEdit.setBackground(new java.awt.Color(255, 255, 255));
+        btnEdit.setFont(new java.awt.Font("Poppins", 1, 14)); // NOI18N
+        btnEdit.setForeground(new java.awt.Color(12, 83, 151));
+        btnEdit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons/ProductEdit.png"))); // NOI18N
+        btnEdit.setText("Editar");
+        btnEdit.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
+        btnEdit.setBorderPainted(false);
+        btnEdit.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnEdit.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btnEditMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btnEditMouseExited(evt);
+            }
+        });
+        btnEdit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEditActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel10Layout = new javax.swing.GroupLayout(jPanel10);
         jPanel10.setLayout(jPanel10Layout);
         jPanel10Layout.setHorizontalGroup(
@@ -270,25 +250,21 @@ public class PageProductsHome extends javax.swing.JPanel {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(btnEdit, javax.swing.GroupLayout.PREFERRED_SIZE, 146, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(btnEditPrice, javax.swing.GroupLayout.PREFERRED_SIZE, 146, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(btnAddStock, javax.swing.GroupLayout.PREFERRED_SIZE, 146, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(btnRerturnProduct, javax.swing.GroupLayout.PREFERRED_SIZE, 146, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(565, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel10Layout.setVerticalGroup(
             jPanel10Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel10Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel10Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel10Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
                     .addComponent(BtnProdHome, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(jPanel10Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(btnEdit, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(btnNewProduct, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(btnEditPrice, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(btnAddStock, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(btnRerturnProduct, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(btnNewProduct, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnEdit, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnAddStock, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnRerturnProduct, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(16, Short.MAX_VALUE))
         );
 
@@ -298,7 +274,7 @@ public class PageProductsHome extends javax.swing.JPanel {
         jPanelProductContent.setLayout(jPanelProductContentLayout);
         jPanelProductContentLayout.setHorizontalGroup(
             jPanelProductContentLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGap(0, 1247, Short.MAX_VALUE)
         );
         jPanelProductContentLayout.setVerticalGroup(
             jPanelProductContentLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -312,7 +288,7 @@ public class PageProductsHome extends javax.swing.JPanel {
             .addComponent(jPanel10, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanelProductContent, javax.swing.GroupLayout.DEFAULT_SIZE, 1471, Short.MAX_VALUE))
+                .addComponent(jPanelProductContent, javax.swing.GroupLayout.DEFAULT_SIZE, 1247, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -336,30 +312,6 @@ public class PageProductsHome extends javax.swing.JPanel {
     private void btnNewProductActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNewProductActionPerformed
 
     }//GEN-LAST:event_btnNewProductActionPerformed
-
-    private void btnEditMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnEditMouseEntered
-        btnEdit.setBackground(new Color(180,180,180));
-    }//GEN-LAST:event_btnEditMouseEntered
-
-    private void btnEditMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnEditMouseExited
-        btnEdit.setBackground(new Color(255,255,255));
-    }//GEN-LAST:event_btnEditMouseExited
-
-    private void btnEditActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEditActionPerformed
-
-    }//GEN-LAST:event_btnEditActionPerformed
-
-    private void btnEditPriceMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnEditPriceMouseEntered
-        btnEditPrice.setBackground(new Color(180,180,180));
-    }//GEN-LAST:event_btnEditPriceMouseEntered
-
-    private void btnEditPriceMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnEditPriceMouseExited
-        btnEditPrice.setBackground(new Color(255,255,255));
-    }//GEN-LAST:event_btnEditPriceMouseExited
-
-    private void btnEditPriceActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEditPriceActionPerformed
-
-    }//GEN-LAST:event_btnEditPriceActionPerformed
 
     private void btnRerturnProductMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnRerturnProductMouseEntered
         btnRerturnProduct.setBackground(new Color(180,180,180));
@@ -386,23 +338,34 @@ public class PageProductsHome extends javax.swing.JPanel {
     }//GEN-LAST:event_btnAddStockActionPerformed
 
     private void BtnProdHomeMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BtnProdHomeMouseEntered
-        // TODO add your handling code here:
+        BtnProdHome.setBackground(new Color(180,180,180));
     }//GEN-LAST:event_BtnProdHomeMouseEntered
 
     private void BtnProdHomeMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BtnProdHomeMouseExited
-        // TODO add your handling code here:
+        BtnProdHome.setBackground(new Color(255,255,255));
     }//GEN-LAST:event_BtnProdHomeMouseExited
 
     private void BtnProdHomeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnProdHomeActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_BtnProdHomeActionPerformed
 
+    private void btnEditMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnEditMouseEntered
+        btnEdit.setBackground(new Color(180,180,180));
+    }//GEN-LAST:event_btnEditMouseEntered
+
+    private void btnEditMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnEditMouseExited
+        btnEdit.setBackground(new Color(255,255,255));
+    }//GEN-LAST:event_btnEditMouseExited
+
+    private void btnEditActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEditActionPerformed
+
+    }//GEN-LAST:event_btnEditActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton BtnProdHome;
     private javax.swing.JButton btnAddStock;
     private javax.swing.JButton btnEdit;
-    private javax.swing.JButton btnEditPrice;
     private javax.swing.JButton btnNewProduct;
     private javax.swing.JButton btnRerturnProduct;
     private javax.swing.JPanel jPanel10;
