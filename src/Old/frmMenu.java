f03f3be0cccf1e60c0c87a96cb14b000866cd347
Alehm86 <alehmartin1986@gmail.com>
2026-01-19 17:00:16 -0300

    V1.2 - Modificaciones en formulario, modulo productos

---
+++ src/Old/frmMenu.java
@@ -0,0 +1,363 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package Old;
+
+import forms.frmSalesManagement;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class frmMenu extends javax.swing.JFrame {
+
+    fondoPanel background = new fondoPanel();
+    
+
+    public frmMenu() {
+        
+        this.setContentPane(background); 
+        initComponents();
+        this.setExtendedState(6);
+        
+        actionButtons();
+    }
+
+    class fondoPanel extends JPanel{
+        private Image imagen;      
+        @Override
+        public void paint(Graphics g)
+        {
+            imagen = new ImageIcon(getClass().getResource("/Wallpapers/wallpaperMenu.jpg")).getImage();          
+            g.drawImage(imagen,0, 0, getWidth(), getHeight(),this);         
+            setOpaque(false);      
+            super.paint(g);
+        }
+        
+    }
+    
+    private void actionButtons(){
+        
+        btnProducts.addActionListener(e -> {
+            frmProducts products = new frmProducts();
+            products.setVisible(true);
+            this.dispose();
+        });
+        
+        btnExit.addActionListener(e -> {         
+            this.dispose();
+        });
+        
+        btnSales.addActionListener(e -> {
+            frmSalesManagement sales = new frmSalesManagement();
+            sales.setVisible(true);
+            this.dispose();
+        });
+        
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        btnServiceTec = new javax.swing.JButton();
+        btnCheckingAccount = new javax.swing.JButton();
+        btnSales = new javax.swing.JButton();
+        btnCustomers = new javax.swing.JButton();
+        btnProducts = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
+        btnExit = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("HM Gestión");
+        setUndecorated(true);
+        setResizable(false);
+
+        btnServiceTec.setBackground(new java.awt.Color(13, 155, 219));
+        btnServiceTec.setFont(new java.awt.Font("Poppins", 0, 14)); // NOI18N
+        btnServiceTec.setForeground(new java.awt.Color(255, 255, 255));
+        btnServiceTec.setText("SERVICIO TECNICO");
+        btnServiceTec.setBorder(null);
+        btnServiceTec.setBorderPainted(false);
+        btnServiceTec.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnServiceTec.setFocusPainted(false);
+        btnServiceTec.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btnServiceTecMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btnServiceTecMouseExited(evt);
+            }
+        });
+        btnServiceTec.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnServiceTecActionPerformed(evt);
+            }
+        });
+
+        btnCheckingAccount.setBackground(new java.awt.Color(13, 155, 219));
+        btnCheckingAccount.setFont(new java.awt.Font("Poppins", 0, 14)); // NOI18N
+        btnCheckingAccount.setForeground(new java.awt.Color(255, 255, 255));
+        btnCheckingAccount.setText("CUENTAS CORRIENTES");
+        btnCheckingAccount.setBorder(null);
+        btnCheckingAccount.setBorderPainted(false);
+        btnCheckingAccount.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnCheckingAccount.setFocusPainted(false);
+        btnCheckingAccount.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btnCheckingAccountMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btnCheckingAccountMouseExited(evt);
+            }
+        });
+        btnCheckingAccount.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCheckingAccountActionPerformed(evt);
+            }
+        });
+
+        btnSales.setBackground(new java.awt.Color(13, 155, 219));
+        btnSales.setFont(new java.awt.Font("Poppins", 0, 24)); // NOI18N
+        btnSales.setForeground(new java.awt.Color(255, 255, 255));
+        btnSales.setText("VENTAS");
+        btnSales.setBorder(null);
+        btnSales.setBorderPainted(false);
+        btnSales.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnSales.setFocusPainted(false);
+        btnSales.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btnSalesMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btnSalesMouseExited(evt);
+            }
+        });
+        btnSales.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSalesActionPerformed(evt);
+            }
+        });
+
+        btnCustomers.setBackground(new java.awt.Color(13, 155, 219));
+        btnCustomers.setFont(new java.awt.Font("Poppins", 0, 24)); // NOI18N
+        btnCustomers.setForeground(new java.awt.Color(255, 255, 255));
+        btnCustomers.setText("CLIENTES");
+        btnCustomers.setBorder(null);
+        btnCustomers.setBorderPainted(false);
+        btnCustomers.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnCustomers.setFocusPainted(false);
+        btnCustomers.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btnCustomersMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btnCustomersMouseExited(evt);
+            }
+        });
+        btnCustomers.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCustomersActionPerformed(evt);
+            }
+        });
+
+        btnProducts.setBackground(new java.awt.Color(13, 155, 219));
+        btnProducts.setFont(new java.awt.Font("Poppins", 0, 24)); // NOI18N
+        btnProducts.setForeground(new java.awt.Color(255, 255, 255));
+        btnProducts.setText("PRODUCTOS");
+        btnProducts.setBorder(null);
+        btnProducts.setBorderPainted(false);
+        btnProducts.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnProducts.setFocusPainted(false);
+        btnProducts.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btnProductsMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btnProductsMouseExited(evt);
+            }
+        });
+        btnProducts.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnProductsActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(12, 12, 12)
+                .addComponent(btnProducts, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnSales, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btnCustomers, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btnCheckingAccount, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnServiceTec, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnServiceTec, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(btnSales, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btnCustomers, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btnProducts, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(btnCheckingAccount, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255,0));
+
+        btnExit.setBackground(new java.awt.Color(255, 255, 255));
+        btnExit.setFont(new java.awt.Font("Poppins", 1, 24)); // NOI18N
+        btnExit.setForeground(new java.awt.Color(101, 129, 171));
+        btnExit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons/exit2_64.png"))); // NOI18N
+        btnExit.setText("Salir");
+        btnExit.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnExit.setFocusPainted(false);
+        btnExit.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btnExitMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btnExitMouseExited(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(382, Short.MAX_VALUE)
+                .addComponent(btnExit, javax.swing.GroupLayout.PREFERRED_SIZE, 185, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(372, 372, 372))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(123, Short.MAX_VALUE)
+                .addComponent(btnExit)
+                .addGap(24, 24, 24))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(500, 500, 500)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(76, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(443, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(70, 70, 70))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnCheckingAccountActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCheckingAccountActionPerformed
+    }//GEN-LAST:event_btnCheckingAccountActionPerformed
+
+    private void btnServiceTecActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnServiceTecActionPerformed
+    }//GEN-LAST:event_btnServiceTecActionPerformed
+
+    private void btnSalesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSalesActionPerformed
+    }//GEN-LAST:event_btnSalesActionPerformed
+
+    private void btnCustomersActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCustomersActionPerformed
+    }//GEN-LAST:event_btnCustomersActionPerformed
+
+    private void btnProductsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnProductsActionPerformed
+        
+    }//GEN-LAST:event_btnProductsActionPerformed
+
+    private void btnProductsMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnProductsMouseEntered
+        btnProducts.setBackground(new Color(13,120,219));
+    }//GEN-LAST:event_btnProductsMouseEntered
+
+    private void btnProductsMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnProductsMouseExited
+        btnProducts.setBackground(new Color(13,155,219));
+    }//GEN-LAST:event_btnProductsMouseExited
+
+    private void btnSalesMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnSalesMouseEntered
+        btnSales.setBackground(new Color(13,120,219));
+    }//GEN-LAST:event_btnSalesMouseEntered
+
+    private void btnSalesMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnSalesMouseExited
+        btnSales.setBackground(new Color(13,155,219));
+    }//GEN-LAST:event_btnSalesMouseExited
+
+    private void btnCustomersMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnCustomersMouseEntered
+        btnCustomers.setBackground(new Color(13,120,219));
+    }//GEN-LAST:event_btnCustomersMouseEntered
+
+    private void btnCustomersMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnCustomersMouseExited
+        btnCustomers.setBackground(new Color(13,155,219));
+    }//GEN-LAST:event_btnCustomersMouseExited
+
+    private void btnCheckingAccountMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnCheckingAccountMouseEntered
+        btnCheckingAccount.setBackground(new Color(13,120,219));
+    }//GEN-LAST:event_btnCheckingAccountMouseEntered
+
+    private void btnCheckingAccountMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnCheckingAccountMouseExited
+        btnCheckingAccount.setBackground(new Color(13,155,219));
+    }//GEN-LAST:event_btnCheckingAccountMouseExited
+
+    private void btnServiceTecMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnServiceTecMouseEntered
+        btnServiceTec.setBackground(new Color(13,120,219));
+    }//GEN-LAST:event_btnServiceTecMouseEntered
+
+    private void btnServiceTecMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnServiceTecMouseExited
+        btnServiceTec.setBackground(new Color(13,155,219));
+    }//GEN-LAST:event_btnServiceTecMouseExited
+
+    private void btnExitMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnExitMouseEntered
+        btnExit.setBackground(new Color(238,238,238));
+    }//GEN-LAST:event_btnExitMouseEntered
+
+    private void btnExitMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnExitMouseExited
+        btnExit.setBackground(new Color(255,255,255));
+    }//GEN-LAST:event_btnExitMouseExited
+
+
+    public static void main(String args[]) {
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new frmMenu().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCheckingAccount;
+    private javax.swing.JButton btnCustomers;
+    private javax.swing.JButton btnExit;
+    private javax.swing.JButton btnProducts;
+    private javax.swing.JButton btnSales;
+    private javax.swing.JButton btnServiceTec;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    // End of variables declaration//GEN-END:variables
+}
