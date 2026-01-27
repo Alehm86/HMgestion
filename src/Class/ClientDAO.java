b72262f0d19f2abd879ac94062b45e8a26aaa8a2
Alehm86 <alehmartin1986@gmail.com>
2026-01-27 17:08:10 -0300

    V1.2 - Modificaciones en formulario, modulo productos

--- src/Class/ClientDAO.java
+++ src/Class/ClientDAO.java
@@ -6,7 +6,43 @@ package Class;
 
 import ConnectionDB.ConnectionDB;
 import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import java.security.Timestamp;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import javax.swing.JOptionPane;
+
 
 public class ClientDAO {
+    
+//    REGISTRAR CLIENTE NUEVO
+    public void insertClient(String name,int tipeClient, int CUIT, String email,int phone, String street, int height, int idProvince, String fechaRegistro, int state,int iva){
+        
+        ConnectionDB con = new ConnectionDB();
+        Connection conexion = (Connection) con.establecerConexion();
+        
+        String sql="INSERT INTO `client`(`name`, `tipeClient`, `CUIT`, `email`, `phone`, `street`, `height`, `idProvince`, `fechaRegistro`, `state`, `iva`) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+
+        try{
+            PreparedStatement pstmt = (PreparedStatement) conexion.prepareStatement(sql);
+            pstmt.setString(1, name);
+            pstmt.setInt(2, tipeClient);
+            pstmt.setInt(3, CUIT);
+            pstmt.setString(4, email);
+            pstmt.setInt(5, phone);
+            pstmt.setString(6, street);
+            pstmt.setInt(7, height);
+            pstmt.setInt(8, idProvince);
+            pstmt.setString(9, fechaRegistro);
+            pstmt.setInt(10, state);
+            pstmt.setInt(11, iva);
+            pstmt.executeUpdate();            
+            conexion.close();           
+        }
+        catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "ERROR AL REGISTRAR CLIENTE");
+        }
+
+    }
     
 }
