9431956d5382a7f0819d9e140f529cf26d822dd4
Alehm86 <alehmartin1986@gmail.com>
2026-01-27 17:10:23 -0300

    V1.2 - Modificaciones en formulario, modulo productos

--- src/forms/frmBrandEdit.java
+++ src/forms/frmBrandEdit.java
@@ -6,7 +6,7 @@ package forms;
 
 import Class.GenericDAO;
 import Class.ProductDAO;
-import Class.ModelBrands;
+import Class.modelBrands;
 import java.awt.Color;
 import java.awt.Point;
 import java.awt.event.MouseAdapter;
@@ -26,7 +26,7 @@ public class frmBrandEdit extends javax.swing.JDialog {
     ProductDAO queriesProduct = new ProductDAO();
     GenericDAO queriesGeneric = new GenericDAO();
     
-    ModelBrands classBrand = new ModelBrands();
+    modelBrands classBrand = new modelBrands();
     
     public String marcaSeleccionada = "";
     private String marcaCreada;
