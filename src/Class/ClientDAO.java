3da53ba15fd7966569e41a63bad3dbe8d91b558b
Alehm86 <alehmartin1986@gmail.com>
2026-01-27 16:47:48 -0300

    V1.2 - Modificaciones en formulario, modulo productos

---
+++ src/Class/ClientDAO.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Class;
+
+import ConnectionDB.ConnectionDB;
+import com.mysql.jdbc.Connection;
+
+public class ClientDAO {
+    
+}
